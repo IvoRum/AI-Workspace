b3dc6a963d7148586c4936f143749e18605cfc85
Ivaylo Rumenov <101731035+IvoRum@users.noreply.github.com>
2024-04-26 11:08:43 -0700

    Update Demo4.py

--- chain/DXC_Demo/Demo_4/Demo4.py
+++ chain/DXC_Demo/Demo_4/Demo4.py
@@ -5,7 +5,7 @@ os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
 oracle = pipeline(model="dandelin/vilt-b32-finetuned-vqa", token='TOKEN')
 image_url = "https://huggingface.co/datasets/Narsil/image_dummy/raw/main/lena.png"
-pipe = pipeline("text-classification", token='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')
+pipe = pipeline("text-classification", token='TOKEN')
 oracleItem=pipe(oracle(question="What is she wearing ?", image=image_url))
 
 print(oracleItem)
